8a2160dfe4929d5c0e9f64ecdaeef7abab5bf83d
Koppula Ravi <C005339@portaldom.ent.intranet.rhbbank.com>
2022-09-30 10:55:24 +0800

    Initial commit

---
+++ CrystalReportsInASP.NETWebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,81 @@
+﻿using CrystalReportsInASP.NETWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrystalReportsInASP.NETWebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeReport()
+        {
+            return View();
+        }
+
+        public void GenerateEmployeeReport()
+        {
+            ReportParams<EmployeeInfoEntity> objReportParams = new ReportParams<EmployeeInfoEntity>();
+
+            objReportParams.DataSource = GetAllEmployee();
+            objReportParams.RptFileName = "EmployeeInfReport.rpt";
+
+            this.HttpContext.Session["ReportType"] = "EmployeeInfoReport";
+            this.HttpContext.Session["ReportParam"] = objReportParams;
+        }
+
+        public List<EmployeeInfoEntity> GetAllEmployee()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["SqlConnectionString"].ConnectionString;
+            DataTable dt = new DataTable();
+            string sql = "SELECT * FROM  Employees";
+            SqlConnection con = new SqlConnection(constr);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataAdapter adpt = new SqlDataAdapter(cmd);
+            adpt.Fill(dt);
+
+            var list = ConvertDataTableToList<EmployeeInfoEntity>(dt);
+
+            return list;
+        }
+
+        private static List<T> ConvertDataTableToList<T>(DataTable dt)
+        {
+
+            List<T> data = new List<T>();
+            foreach(DataRow row in dt.Rows)
+            {
+                T item = GetItem<T>(row);
+                data.Add(item);
+            }
+            return data;
+        }
+
+        private static T GetItem<T>(DataRow dr)
+        {
+            Type temp = typeof(T);
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (DataColumn column in dr.Table.Columns)
+            {
+                foreach (PropertyInfo pro in temp.GetProperties())
+                {
+                    if (pro.Name == column.ColumnName)
+                        pro.SetValue(obj, dr[column.ColumnName], null);
+                    else
+                        continue;
+
+                }
+
+            }
+
+            return obj;
+        }
+    }
+}
---
+++ CrystalReportsInASP.NETWebApp/Models/EmployeeInfoEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrystalReportsInASP.NETWebApp.Models
+{
+    public class EmployeeInfoEntity
+    {
+        public long EmployeeID { get; set; }
+        public string EmployeeName { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string MailID { get; set; } = string.Empty;
+        public DateTime DOJ { get; set; }
+
+    }
+}
