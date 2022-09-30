7a8a9d24b26aa5bd9efd85ae0b08011c6de08967
Koppula Ravi <C005339@portaldom.ent.intranet.rhbbank.com>
2022-09-30 17:52:39 +0800

    menu typo changed

---
+++ CrystalReportsInASP.NETWebApp/obj/Release/AspnetCompileMerge/TempBuildDir/Scripts/DepartmentReport/DepartmentReport.js
@@ -0,0 +1,43 @@
+﻿
+
+$(document).ready(function () {
+
+    $("#btnLoadReport").click(function () {
+
+        ReportManager.LoadReport();
+
+    });
+
+});
+
+var ReportManager = {
+
+    LoadReport: function () {
+
+        var jsonParam = "";
+        var serviveUrl = "../Department/GenerateDepartmentReport";
+
+        ReportManager.GetReport(serviveUrl, jsonParam, onFailed);
+
+        function onFailed(error) {
+            alert("Found Error");
+        }
+    },
+
+    GetReport: function (serviceUrl, jsonParams, errorCallback) {
+
+        jQuery.ajax({
+
+            url: serviceUrl,
+            asyn: false,
+            type: "POST",
+            data: "{" + jsonParams + "}",
+            contentType: "application/json;charset-utf-8",
+            success: function () {
+                window.open('../Report/DepartmentReportViewer.aspx', '_newtab');
+            },
+            error: errorCallback
+        });
+
+    }
+}
